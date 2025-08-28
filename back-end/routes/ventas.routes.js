a22a9d5b5456fd930fd1478d98202647c070b448
vianka <viankaandrearojasgonzales@gmail.com>
2025-08-28 09:11:38 -0400

    Subiendo cambios correctos

---
+++ back-end/routes/ventas.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { registrarVenta, listarVentas } from "../controllers/ventas.controller.js";
+
+const router = express.Router();
+
+router.post("/", registrarVenta);
+router.get("/", listarVentas);
+
+export default router;
+4
